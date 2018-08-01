9dc1dfd125e3d3d3e7e6c57d0b77e7ec3bd65478
nitesh581 <niteshkumar581@gmail.com>
2018-08-01 14:00:32 +0530

    some changes in cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,5 +60,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'getUserRole' => \App\Http\Middleware\GetUserRole::class,
         'getUserId' => \App\Http\Middleware\GetUserId::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 }
